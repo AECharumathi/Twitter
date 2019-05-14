6a5fad8ba1d10f181b72ffbdad4e61558cbeea75
AECharumathi <charumathielangovan96@gmail.com>
2019-05-14 23:59:19 +0530

    refactored code using declarative methods

--- Twitter Home Page/js/homePage.js
+++ Twitter Home Page/js/homePage.js
@@ -47,38 +47,41 @@ var list=0;
 			
 				
 	function friend_suggestion(data){
-		for(var index=0;index<data.length;index++){
+		[].forEach.call(data,()=>{
 		var element_div=document.createElement("div");
 		element_div.className="side-container2-body";
 		document.querySelector(".side-container2-b").appendChild(element_div);
-		}
-		var parentDiv=document.querySelectorAll(".side-container2-body");
-	
-		for(var index=0;index<data.length;index++){
-			var element_image=document.createElement("img");
-			element_image.src=data[index].profile_img;
-			parentDiv[index].appendChild(element_image);
-			var userName=document.createElement("span");
-			userName.textContent=data[index].user_name;
-			parentDiv[index].appendChild(userName);	
-			var fullName=document.createElement("span");
-			fullName.className="text-property";
-			fullName.textContent=data[index].full_name;
-			parentDiv[index].appendChild(fullName);	
-			var follow=document.createElement("button");
-			follow.className="button";
-			follow.textContent="Follow";
-			parentDiv[index].appendChild(follow);
-		}
-	}
+        })
+        var parentDiv=document.querySelectorAll(".side-container2-body");
+         [].forEach.call(parentDiv,(sug,i)=>{
+                                [].forEach.call(data,(data,ind)=>{
+                                   if(i==ind){
+                                    var element_image=document.createElement("img");
+                                    element_image.src=data.profile_img;
+                                    sug.appendChild(element_image);
+                                    var userName=document.createElement("span");
+                                    userName.textContent=data.user_name;
+                                    sug.appendChild(userName);	
+                                    var fullName=document.createElement("span");
+                                    fullName.className="text-property";
+                                    fullName.textContent=data.full_name;
+                                    sug.appendChild(fullName);	
+                                    var follow=document.createElement("button");
+                                    follow.className="button";
+                                    follow.textContent="Follow";
+                                    sug.appendChild(follow);
+                                    }
+                                   })
+                                });
 
+    }
 
 	PromiseCall(this.PromiseList[++list])
 			.then(d=>tweet_data(d.data));
 			
 
 	function tweet_data(data){
-		for(var index=0;index<data.length;index++){
+		[].forEach.call(data,dataV=>{
 			var element_div_main=document.createElement("div");
 			element_div_main.className="main-container2";
 			document.querySelector(".main-container-tweet").appendChild(element_div_main);
@@ -90,14 +93,14 @@ var list=0;
 					profile_image.className="avatar"; 
 					document.querySelector(".main-container2-header").appendChild(profile_image);
 					var element_follow=document.createElement("span");
-					element_follow.textContent=data[index].user.full_name;
+					element_follow.textContent=dataV.user.full_name;
 					document.querySelector(".main-container2-header").appendChild(element_follow);	
 					var element_span=document.createElement("span");
 					element_span.className="text-property";
-					element_span.textContent=`@ ${data[index].user.user_name}`;
+					element_span.textContent=`@ ${dataV.user.user_name}`;
 					document.querySelector(".main-container2-header").appendChild(element_span);
 					var element_span1=document.createElement("span");
-					var tweet_date=new Date(data[index].created_at);
+					var tweet_date=new Date(dataV.created_at);
 	  				element_span1.textContent=`${tweet_date.getDate()}-${TweetDate(tweet_date.getMonth())}-${tweet_date.getFullYear()}`;
 	  				element_span1.className="text-property";
 					document.querySelector(".main-container2-header").appendChild(element_span1);
@@ -107,37 +110,37 @@ var list=0;
 					document.querySelector(".main-container2-header").appendChild(toodle_img);
 				var element_div_body=document.createElement("div");
 				element_div_body.className="main-container2-body";
-				element_div_body.textContent=data[index].text;
+				element_div_body.textContent=dataV.text;
 				document.querySelector(".main-container2").appendChild(element_div_body);
-					if(data[index].entities.hasOwnProperty('media')){
-						if(data[index].entities.media[0].type==="video"){
+					if(dataV.entities.hasOwnProperty('media')){
+						if(dataV.entities.media[0].type==="video"){
 							var mediaElem=document.createElement("video");
-							mediaElem.src=data[index].entities.media[0].link;
+							mediaElem.src=dataV.entities.media[0].link;
 							mediaElem.controls=true;
 							mediaElem.className="tweet-video";
 							document.querySelector(".main-container2-body").appendChild(mediaElem);
 						}
-						else if(data[index].entities.media[0].type==="image"){
+						else if(dataV.entities.media[0].type==="image"){
 							var mediaElem=document.createElement("img");
-							mediaElem.src=data[index].entities.media[0].link;
+							mediaElem.src=dataV.entities.media[0].link;
 							document.querySelector(".main-container2-body").appendChild(mediaElem);
 						}
 					}
 			var imgSrc=["Images/comment.png", "Images/refresh.png", "Images/like.png"];
 			var footerImg=[];
 			var element_span_foot=[];
-			var imgValue=[data[index].stats.comments, data[index].stats.retweets, data[index].stats.likes];
+			var imgValue=[dataV.stats.comments, dataV.stats.retweets, dataV.stats.likes];
 			var element_div_foot=document.createElement("div");
 			element_div_foot.className="main-container2-footer";
 			document.querySelector(".main-container2").appendChild(element_div_foot);
 				element_span_foot=document.createElement("span");
 				element_span_foot.className="main-container2-footer-span"
 				document.querySelector(".main-container2-footer").appendChild(element_span_foot);
-					for(var s_index=0; s_index<3;s_index++){
-						footerImg[s_index]=document.createElement("img");
-						footerImg[s_index].src=imgSrc[s_index];
-						document.querySelector(".main-container2-footer-span").appendChild(footerImg[s_index]);
-						document.querySelector(".main-container2-footer-span").appendChild(document.createTextNode(imgValue[s_index]));
-					}
-		}
+                [].forEach.call(imgSrc,function(imgSrc,i){
+                    footerImg[i]=document.createElement("img");
+                    footerImg[i].src=imgSrc;
+						document.querySelector(".main-container2-footer-span").appendChild(footerImg[i]);
+						document.querySelector(".main-container2-footer-span").appendChild(document.createTextNode(imgValue[i]));
+					})
+		})
 	}
--- Twitter Profile Page/js/profilePage.js
+++ Twitter Profile Page/js/profilePage.js
@@ -60,20 +60,20 @@ PromiseCall(this.PromiseList[++list])
 
 function user_media(data){
 	var createMedia=[];
-	for(var index=0;index<data.length;index++){
-		if(data[index].type==="video"){
+	[].forEach.call(data,(mediaFile,i)=>{
+		if(mediaFile.type==="video"){
 				let mediaElem=document.createElement("video");
-						mediaElem.src=data[index].url;
+						mediaElem.src=mediaFile.url;
 						mediaElem.className="side-container1-body-div";
 				  		document.querySelector(".side-container1-body").appendChild(mediaElem);
 			}
-		else if(data[index].type==="image"){
+		else if(mediaFile.type==="image"){
 				let mediaElem=document.createElement("img");
-						mediaElem.src=data[index].url;
+						mediaElem.src=mediaFile.url;
 						mediaElem.className="side-container1-body-div";
 						document.querySelector(".side-container1-body").appendChild(mediaElem);
 			}
-	}
+	})
 }
 
 PromiseCall(this.PromiseList[++list])
@@ -81,36 +81,40 @@ PromiseCall(this.PromiseList[++list])
 						
 function friend_suggestion(data){
 
- 	for(var index=0;index<data.length;index++){
+	[].forEach.call(data,()=>{
 		let element_div=document.createElement("div");
  	 	element_div.className="side-container2-body";
 	  	document.querySelector("#side-container2").appendChild(element_div);
-	}
+	})
 	let parentDiv=document.querySelectorAll(".side-container2-body");
 	
-	for(var index=0;index<data.length;index++){ 
-	  let element_image=document.createElement("img");
-	  	  element_image.src=data[index].profile_img;
-	  	  parentDiv[index].appendChild(element_image);
-	  let userName=document.createElement("span");
-	  	  userName.textContent=data[index].user_name;
-	  	  parentDiv[index].appendChild(userName);	
-	  let fullName=document.createElement("div");
-	  	  fullName.className="text-property";
-	  	  fullName.textContent=data[index].full_name;
-	  	  parentDiv[index].appendChild(fullName);	
-	  let follow=document.createElement("button");
-	  	  follow.className="button";
-	      follow.textContent="Follow";
-	  	  parentDiv[index].appendChild(follow);
-	}
+	[].forEach.call(parentDiv,(sug,i)=>{
+		[].forEach.call(data,(data,ind)=>{
+		   if(i==ind){
+	  			let element_image=document.createElement("img");
+	  	  		element_image.src=data.profile_img;
+	  	  		sug.appendChild(element_image);
+	  			let userName=document.createElement("span");
+	  	  		userName.textContent=data.user_name;
+	  	  		sug.appendChild(userName);	
+	  			let fullName=document.createElement("div");
+	  	  		fullName.className="text-property";
+	  	  		fullName.textContent=data.full_name;
+	  	  		sug.appendChild(fullName);	
+	  			let follow=document.createElement("button");
+	  	  		follow.className="button";
+	      		follow.textContent="Follow";
+				sug.appendChild(follow);
+		}
+	})
+  })
 }
 
 PromiseCall(this.PromiseList[++list])
 			.then(d=>tweet_data(d.data));
 
 function tweet_data(data){
-	for(var index=0;index<data.length;index++){
+	[].forEach.call(data,dataV=>{
 	  let div_main=document.createElement("div");
  	  div_main.className="main-container2";
 	  document.querySelector(".main-container-tweet").appendChild(div_main);
@@ -122,14 +126,14 @@ function tweet_data(data){
 	      	profile_image.className="avatar"; 
 	      	document.querySelector(".main-container2-header").appendChild(profile_image);
 	  		let element_follow=document.createElement("span");
-	  	  	element_follow.textContent=data[index].user.full_name;
+	  	  	element_follow.textContent=dataV.user.full_name;
 	  	  	document.querySelector(".main-container2-header").appendChild(element_follow);	
 	  		let element_span=document.createElement("span");
 	  	  	element_span.className="text-property";
-	  	  	element_span.textContent=`@ ${data[index].user.user_name}`;
+	  	  	element_span.textContent=`@ ${dataV.user.user_name}`;
 	  	  	document.querySelector(".main-container2-header").appendChild(element_span);
 	  		let element_span1=document.createElement("span");
-	  	  	let tweet_date=new Date(data[index].created_at);
+	  	  	let tweet_date=new Date(dataV.created_at);
 	  			tweet_date = `${tweet_date.getDate()}-${TweetDate(tweet_date.getMonth())}-${tweet_date.getFullYear()}`;
 	  			element_span1.textContent=tweet_date;
 	  			element_span1.className="text-property";
@@ -140,170 +144,182 @@ function tweet_data(data){
 	  	  	document.querySelector(".main-container2-header").appendChild(toodle_img);
 	  	let div_body=document.createElement("div");
  	  	div_body.className="main-container2-body";
-	  	div_body.textContent=data[index].text;
+	  	div_body.textContent=dataV.text;
 	   	document.querySelector(".main-container2").appendChild(div_body);
-		 	if(data[index].entities.hasOwnProperty('media')){
-				if(data[index].entities.media[0].type==="video"){
+		 	if(dataV.entities.hasOwnProperty('media')){
+				if(dataV.entities.media[0].type==="video"){
 					let mediaElem=document.createElement("video");
-						mediaElem.src=data[index].entities.media[0].link;
+						mediaElem.src=dataV.entities.media[0].link;
 						mediaElem.controls=true;
 						mediaElem.className="tweet-video";
 						document.querySelector(".main-container2-body").appendChild(mediaElem);
-				} else if(data[index].entities.media[0].type==="image"){
+				} else if(dataV.entities.media[0].type==="image"){
 					let mediaElem=document.createElement("img");
-						mediaElem.src=data[index].entities.media[0].link;
+						mediaElem.src=dataV.entities.media[0].link;
 						document.querySelector(".main-container2-body").appendChild(mediaElem);
 				}
 		  	}
 		let imgSrc=["Images/comment.png", "Images/refresh.png", "Images/like.png"];
 		let footerImg=[];
 		let element_span_foot=[];
-		let imgValue=[data[index].stats.comments, data[index].stats.retweets, data[index].stats.likes];
+		let imgValue=[dataV.stats.comments, dataV.stats.retweets, dataV.stats.likes];
 	  	let element_div_foot=document.createElement("div");
  	  		element_div_foot.className="main-container2-footer";
  	  		document.querySelector(".main-container2").appendChild(element_div_foot);
  	  		element_span_foot=document.createElement("span");
  	  		element_span_foot.className="main-container2-footer-span"
  	  		document.querySelector(".main-container2-footer").appendChild(element_span_foot);
- 	   		for(var s_index=0; s_index<3;s_index++){
- 	   			footerImg[s_index]=document.createElement("img");
-	  			footerImg[s_index].src=imgSrc[s_index];
-	  			document.querySelector(".main-container2-footer-span").appendChild(footerImg[s_index]);
-	  			document.querySelector(".main-container2-footer-span").appendChild(document.createTextNode(imgValue[s_index]));
-	  		}
-	}
+			   [].forEach.call(imgSrc,function(imgSrc,i){
+				footerImg[i]=document.createElement("img");
+				footerImg[i].src=imgSrc;
+					document.querySelector(".main-container2-footer-span").appendChild(footerImg[i]);
+					document.querySelector(".main-container2-footer-span").appendChild(document.createTextNode(imgValue[i]));
+				})
+	})
 }
 
 PromiseCall(this.PromiseList[++list])
 			.then(d=>following_data(d.data));
 
 function following_data(data){
-	for(var index=0;index<data.length;index++){
+	[].forEach.call(data,dataV=>{
 	  let mainBlock=document.createElement("div");
  	  mainBlock.className="Following-container1";
 	  document.querySelector(".Following-container-body").appendChild(mainBlock);
-	  }
+	  })
 	  let parentDiv=document.querySelectorAll(".Following-container1");
 	  
-	  for(var index=0;index<data.length;index++){
+	  [].forEach.call(parentDiv,(parent,i)=>{
 	  	var paddingDiv=document.createElement("div");
 	  	paddingDiv.className="Following-container1-padding";
-	  	parentDiv[index].appendChild(paddingDiv);
-		}
+	  	parent.appendChild(paddingDiv);
+		})
 	  var paddingDiv=document.querySelectorAll(".Following-container1-padding");
-	  for(var index=0;index<data.length;index++){
-	  if(data[index].cover_img!=""){
+	  [].forEach.call(paddingDiv,(padding,i)=>{
+	  if(data[i].cover_img!=""){
 	  		let headerImg=document.createElement("img");
-	  		headerImg.src=data[index].cover_img;
+	  		headerImg.src=data[i].cover_img;
 	  		headerImg.className="Following-container1-header";
-	  		paddingDiv[index].appendChild(headerImg);
+	  		padding.appendChild(headerImg);
 			}
 			else{
 				let headerImg=document.createElement("img");
 				headerImg.src="Images/DefaultCoverImage.png";
 				headerImg.className="Following-container1-header";
-				paddingDiv[index].appendChild(headerImg);
+				padding.appendChild(headerImg);
 			}
 	  		let body=document.createElement("div");
 	  		body.className="Following-container1-body";
-	  		paddingDiv[index].appendChild(body);
+	  		padding.appendChild(body);
 	   		let footer=document.createElement("div");
 	  		footer.className="Following-container1-footer";
-	  		paddingDiv[index].appendChild(footer);
-	}  			
+	  		padding.appendChild(footer);
+	})  			
 	 let body=document.querySelectorAll(".Following-container1-body");
 	 let footer=document.querySelectorAll(".Following-container1-footer");
-	  for(var index=0;index<data.length;index++){
-		let profile_img=document.createElement("img");
-	  	profile_img.src=data[index].profile_img;
-	  	body[index].appendChild(profile_img);
-	  	let body_in=document.createElement("div");
-	  	body_in.className="Following-container1-body-inside";
-	  	body[index].appendChild(body_in);
-		let footerContent=document.createElement("div");
-	  	footer[index].appendChild(footerContent);
-	}
+	 [].forEach.call(body,(body,i)=>{
+		[].forEach.call(footer,(footer,ind)=>{
+		   if(i==ind){
+				let profile_img=document.createElement("img");
+	  			profile_img.src=data[i].profile_img;
+	  			body.appendChild(profile_img);
+	  			let body_in=document.createElement("div");
+	  			body_in.className="Following-container1-body-inside";
+	  			body.appendChild(body_in);
+				let footerContent=document.createElement("div");
+	  			footer.appendChild(footerContent);
+			}
+	})})
 	let body_in=document.querySelectorAll(".Following-container1-body-inside");
 	 let footerContent=document.querySelectorAll(".Following-container1-footer div");
-	 for(var index=0;index<data.length;index++){
-	  	let fullName=document.createElement("h3");
-	  	fullName.textContent=data[index].full_name;
-	  	body_in[index].appendChild(fullName);
-	  	let userName=document.createElement("span");
-	  	userName.className="text-property";
-	  	userName.textContent=`@ ${data[index].user_name}`;
-	  	body_in[index].appendChild(userName);
-		let user_bio=document.createElement("p");
-	  	user_bio.className="text-property";
-	  	user_bio.textContent=data[index].user_bio;
-	  	footerContent[index].appendChild(user_bio);
-		}
+	 [].forEach.call(body_in,(body_in,i)=>{
+		[].forEach.call(footerContent,(footc,ind)=>{
+		   if(i==ind){
+			    	let fullName=document.createElement("h3");
+	  				fullName.textContent=data[i].full_name;
+	  				body_in.appendChild(fullName);
+	  				let userName=document.createElement("span");
+	  				userName.className="text-property";
+	  				userName.textContent=`@ ${data[i].user_name}`;
+	  				body_in.appendChild(userName);
+					let user_bio=document.createElement("p");
+	  				user_bio.className="text-property";
+	  				user_bio.textContent=data[i].user_bio;
+	  				footc.appendChild(user_bio);
+			}
+		})
+	})
 }
-
 PromiseCall(this.PromiseList[++list])
 			.then(d=>followers_data(d.data));
 
 function followers_data(data){
-	for(var index=0;index<data.length;index++){
+	[].forEach.call(data,dataV=>{
 	  let mainBlock=document.createElement("div");
  	  mainBlock.className="Followers-container1";
 	  document.querySelector(".Followers-container-body").appendChild(mainBlock);
-	  }
+	  })
 	  let parentDiv=document.querySelectorAll(".Followers-container1");
 	  
-	  for(var index=0;index<data.length;index++){
+	  [].forEach.call(parentDiv,(parent,i)=>{
 	  	let paddingDiv=document.createElement("div");
 	  	paddingDiv.className="Followers-container1-padding";
-	  	parentDiv[index].appendChild(paddingDiv);
-		}
+	  	parent.appendChild(paddingDiv);
+		})
 	  let paddingDiv=document.querySelectorAll(".Followers-container1-padding");
-	  for(var index=0;index<data.length;index++){
-			if(data[index].cover_img!=""){
+	  [].forEach.call(paddingDiv,(padding,i)=>{
+			if(data[i].cover_img!=""){
 	  		let headerImg=document.createElement("img");
-	  		headerImg.src=data[index].cover_img;
+	  		headerImg.src=data[i].cover_img;
 	  		headerImg.className="Followers-container1-header";
-	  		paddingDiv[index].appendChild(headerImg);
+	  		padding.appendChild(headerImg);
 			}
 			else{
 				let headerImg=document.createElement("img");
 				headerImg.src="Images/DefaultCoverImage.png";
 				headerImg.className="Followers-container1-header";
-				paddingDiv[index].appendChild(headerImg);
+				padding.appendChild(headerImg);
 			}
 	  		let body=document.createElement("div");
 	  		body.className="Followers-container1-body";
-	  		paddingDiv[index].appendChild(body);
+	  		padding.appendChild(body);
 	   		let footer=document.createElement("div");
 	  		footer.className="Followers-container1-footer";
-	  		paddingDiv[index].appendChild(footer);
-	  			
-	  				
-	}
-	 let body=document.querySelectorAll(".Followers-container1-body");
-	 let footer=document.querySelectorAll(".Followers-container1-footer");
-	  for(var index=0;index<data.length;index++){
-		let profile_img=document.createElement("img");
-	  	profile_img.src=data[index].profile_img;
-	  	body[index].appendChild(profile_img);
-	  	let body_in=document.createElement("div");
-	  	body_in.className="Followers-container1-body-inside";
-	  	body[index].appendChild(body_in);
-		let footerContent=document.createElement("div");
-	  	footer[index].appendChild(footerContent);
-	}
+	  		padding.appendChild(footer);	  				
+	})
+	let body=document.querySelectorAll(".Followers-container1-body");
+	let footer=document.querySelectorAll(".Followers-container1-footer");
+		[].forEach.call(body,(body,i)=>{
+		[].forEach.call(footer,(footer,ind)=>{
+		   if(i==ind){
+			   	let profile_img=document.createElement("img");
+	  			profile_img.src=data[i].profile_img;
+	  			body.appendChild(profile_img);
+	  			let body_in=document.createElement("div");
+	  			body_in.className="Followers-container1-body-inside";
+	  			body.appendChild(body_in);
+				let footerContent=document.createElement("div");
+	  			footer.appendChild(footerContent);
+			}
+		})
+	})
 	let body_in=document.querySelectorAll(".Followers-container1-body-inside");
 	 let footerContent=document.querySelectorAll(".Followers-container1-footer div");
-	 for(var index=0;index<data.length;index++){
-	  	let fullName=document.createElement("h3");
-	  	fullName.textContent=data[index].full_name;
-	  	body_in[index].appendChild(fullName);
-	  	let userName=document.createElement("span");
-	  	userName.className="text-property";
-	  	userName.textContent=`@ ${data[index].user_name}`;
-	  	body_in[index].appendChild(userName);
-		let user_bio=document.createElement("p");
-	  	user_bio.className="text-property";
-	  	user_bio.textContent=data[index].user_bio;
-	  	footerContent[index].appendChild(user_bio);
-		}
+	 [].forEach.call(body_in,(body_in,i)=>{
+		[].forEach.call(footerContent,(footc,ind)=>{
+		   if(i==ind){
+				let fullName=document.createElement("h3");
+     			fullName.textContent=data[i].full_name;
+				body_in.appendChild(fullName);
+				let userName=document.createElement("span");
+	  			userName.className="text-property";
+	  			userName.textContent=`@ ${data[i].user_name}`;
+	  			body_in.appendChild(userName);
+				let user_bio=document.createElement("p");
+	  			user_bio.className="text-property";
+	  			user_bio.textContent=data[i].user_bio;
+	  			footc.appendChild(user_bio);
+			}
+		})
+	})
 }
